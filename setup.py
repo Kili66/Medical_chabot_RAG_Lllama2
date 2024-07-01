b85c4962fa03cb04d02b252dca78af6f1f1f97fc
Kili66 <mariamkilibech@gmail.com>
2024-07-01 15:17:59 +0300

    setup created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Mari',
+    author_email= 'mari@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
